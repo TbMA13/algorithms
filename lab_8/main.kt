2d7d3418aa9ccb41ded0c461cbe5c4f389e88f5f
chandrianin <TbMA39@yandex.ru>
2023-10-01 22:36:36 +0200

    поразрядная сортировка готова

--- lab_8/main.kt
+++ lab_8/main.kt
@@ -6,7 +6,6 @@ import kotlin.math.pow
 //№8 Поразрядная
 
 fun radixSort(array: Array<Int>) {
-//    TODO проход для отрицательных чисел
     var maxNumberLen = 0
     for (number in array) {
         if (number.toString().length > maxNumberLen) {
@@ -15,7 +14,7 @@ fun radixSort(array: Array<Int>) {
     }
 
     var basesSizes: Array<Int> = Array(10) { 0 }
-    var bases: Array<Array<Int?>> = Array(10) { Array(10) { null } }
+    var bases: Array<Array<Int?>> = Array(10) { Array(array.size) { null } }
     for (i in 0..<maxNumberLen) {
         for (number in array) {
             val currentBase = abs((number / 10.0.pow(i).toInt()) % (10))
@@ -23,23 +22,42 @@ fun radixSort(array: Array<Int>) {
             basesSizes[currentBase]++
         }
         var count = 0
-        for (tempArray in bases){
-            for (number in tempArray){
-                if (number != null){
+        for (tempArray in bases) {
+            for (number in tempArray) {
+                if (number != null) {
                     array[count] = number
                     count++
                 }
             }
         }
         basesSizes = Array(10) { 0 }
-        bases = Array(10) { Array(10) { null } }
+        bases = Array(10) { Array(array.size) { null } }
+    }
+    basesSizes[0] = array.size - 1
+    for (number in array) {
+        if (number < 0) {
+            bases[0][basesSizes[0]] = number
+            basesSizes[0]--
+        } else {
+            bases[1][basesSizes[1]] = number
+            basesSizes[1]++
+        }
+    }
+    var count = 0
+    for (tempArray in bases) {
+        for (number in tempArray) {
+            if (number != null) {
+                array[count] = number
+                count++
+            }
+        }
     }
 }
 
 fun main() {
-    val sortingArray = arrayOf(1, 23, 643, 2, 67838)
+    val sortingArray = arrayOf(1, -23, -643, 2, 67838, -9)
     radixSort(sortingArray)
-    for (number in sortingArray){
+    for (number in sortingArray) {
         println(number)
     }
 }
